2ac4ee4ea7d495e9162726a45318ce2192f69956
Rohit Vipin Mathews <rmathews@agvance.net>
2017-01-18 15:39:00 -0600

    ading swagger

--- src/DataAsService/Startup.cs
+++ src/DataAsService/Startup.cs
@@ -42,6 +42,8 @@ namespace DataAsService
             services.AddTransient<IConnectionFactory, SqlConnectionFactory>();
             services.AddMvc()
                 .AddXmlSerializerFormatters();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,13 @@ namespace DataAsService
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
+            app.UseSwaggerUi();
         }
     }
 }
