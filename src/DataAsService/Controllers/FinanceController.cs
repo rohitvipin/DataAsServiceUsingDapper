9ec81668630f383351e89ef74df47935731717eb
Rohit Vipin Mathews <rmathews@agvance.net>
2017-01-18 13:39:20 -0600

    use dynamic content path

--- src/DataAsService/Controllers/FinanceController.cs
+++ src/DataAsService/Controllers/FinanceController.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -8,17 +10,24 @@ namespace DataAsService.Controllers
     [Route("api/[controller]")]
     public class FinanceController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public FinanceController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         // GET: api/values
         [HttpGet]
         public IActionResult Get()
         {
             try
             {
-                const string filename = "financial1.xml";
-                const string filepath = @"C:\Projects\DataAsService\src\DataAsService\Content";
+                const string fileName = "financial1.xml";
+                const string folderName = @"Content";
 
-                var fileBytes = System.IO.File.ReadAllBytes(Path.Combine(filepath, filename));
-                return File(fileBytes, "application/xml", filename);
+                var fileBytes = System.IO.File.ReadAllBytes(Path.Combine(_hostingEnvironment?.ContentRootPath, folderName, fileName));
+                return File(fileBytes, "application/xml", fileName);
             }
             catch (System.Exception exception)
             {
